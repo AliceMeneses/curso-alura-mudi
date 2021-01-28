2056a3f146dbc4ed44f8998158a5d6e402a10ca6
Alice <alicesilvademeneses@gmail.com>
2021-01-28 18:56:50 -0300

    Usando Spring Data Jpa com o PedidoRepository

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -6,14 +6,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@PersistenceContext
 	private EntityManager entityManager;
@@ -21,9 +26,7 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		TypedQuery<Pedido> query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		
-		List<Pedido> pedidos = query.getResultList();
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		
 		model.addAttribute("pedidos", pedidos);
 		
