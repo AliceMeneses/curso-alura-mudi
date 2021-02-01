c73ccd857fc4a282e1e0ca407d3d729ad9cdc65e
AliceMeneses <alicesilvademeneses@gmail.com>
2021-02-01 10:20:44 -0300

    Adiciona getter e setter do atributo user

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -18,17 +18,17 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataDaEntrega;
 	private String urlProduto;
 	private String urlImagem;
 	private String descricao;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private User user;
-	
+
 	@Enumerated(EnumType.STRING)
 	private StatusPedido status;
 
@@ -86,6 +86,14 @@ public class Pedido {
 
 	public void setStatus(StatusPedido status) {
 		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 }
