d6601d7154b37ab84511e0abe9c6132e2868251b
AliceMeneses <alicesilvademeneses@gmail.com>
2021-02-01 09:34:24 -0300

    Adiciona método findByUser()

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.model.User;
 
 public interface PedidoRepository  extends JpaRepository<Pedido, Long>{
 
 	List<Pedido> findByStatus(StatusPedido status);
+	
+	List<Pedido> findByUser(User user);
 
 }
