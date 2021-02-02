28f2d7b8d7d5d58906b75781c7982c6e6e74cd30
AliceMeneses <alicesilvademeneses@gmail.com>
2021-02-02 08:45:23 -0300

    Adiciona método findByStatusAndUser()

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -3,6 +3,8 @@ package br.com.alura.mvc.mudi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
@@ -13,5 +15,8 @@ public interface PedidoRepository  extends JpaRepository<Pedido, Long>{
 	List<Pedido> findByStatus(StatusPedido status);
 	
 	List<Pedido> findByUser(User user);
+
+	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username and p.status = :status")
+	List<Pedido> findByStatusAndUser(StatusPedido status, @Param("username") String username);
 
 }
