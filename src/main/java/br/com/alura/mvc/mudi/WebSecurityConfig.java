75d0700ea5a8dc0388b70268de99bbb34faed983
AliceMeneses <alicesilvademeneses@gmail.com>
2021-02-02 14:57:53 -0300

    Troca o caminho para a página que será carregada depois do login para /usuario/pedido

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -3,19 +3,12 @@ package br.com.alura.mvc.mudi;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import net.bytebuddy.description.annotation.AnnotationValue.ForEnumerationDescription.WithUnknownConstant;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.and()
 			.formLogin(form -> form
 				.loginPage("/login")
-				.defaultSuccessUrl("/home", true)
+				.defaultSuccessUrl("/usuario/pedido", true)
 				.permitAll()
 			)
 			.logout(logout -> logout.logoutUrl("/logout"))
